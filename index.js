9063ba0e1aec770b1ff4de96bba28b755c716b7c
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-07 19:18:56 +0800

    add handler for adding person with duplicate name

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { GraphQLError } = require('graphql');
 const { v1: uuid } = require('uuid');
 
 let persons = [
@@ -70,6 +71,16 @@ const resolvers = {
   },
   Mutation: {
     addPerson: (root, args) => {
+      // Handle case of duplicate name
+      if (persons.find((p) => p.name === args.name)) {
+        throw new GraphQLError('Name must be unique', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+          },
+        });
+      }
+
       const person = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
