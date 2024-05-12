aa04f65d124e27693cfe04aff8cb6ca6077d130c
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-12 16:44:46 +0800

    add create user and login mutations

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,5 +1,8 @@
 const { GraphQLError } = require('graphql');
 const Person = require('../models/person');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../utils/config')
 
 const resolvers = {
   Query: {
@@ -39,6 +42,7 @@ const resolvers = {
       }
       return person;
     },
+
     editNumber: async (root, args) => {
       const person = await Person.findOne({ name: args.name });
       person.phone = args.phone;
@@ -55,6 +59,39 @@ const resolvers = {
         });
       }
     },
+
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username });
+
+      return user.save().catch((err) => {
+        throw new GraphQLError('Create new user failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.username,
+            err,
+          },
+        });
+      });
+    },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if ( !user || args.password !== 'secret') {
+        throw new GraphQLError('wrong credentails', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET)}
+    }
   },
 };
 
