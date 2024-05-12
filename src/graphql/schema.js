203c2cacec52a7e819cf6e587cfcba18d3087239
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-12 16:17:06 +0800

    gql schema is now its own separate module

---
+++ src/graphql/schema.js
@@ -0,0 +1,42 @@
+const typeDefs = `#graphql
+  enum YesNo {
+    YES
+    NO
+  }
+
+  type Address {
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons(phone: YesNo): [Person!]!
+    findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
+  }
+`;
+
+module.exports = {
+  typeDefs,
+};
