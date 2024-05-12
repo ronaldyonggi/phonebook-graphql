12cbaba7fab82649058adc3d6306d6091bc4c98f
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-12 16:17:43 +0800

    configuration for mongDB connection

---
+++ src/utils/db.js
@@ -0,0 +1,17 @@
+// Initialize MongoDB Connection
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const connectToDatabase = () => {
+  mongoose.set('strictQuery', false);
+  console.log('connecting to', config.MONGODB_URI);
+
+  mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('error connecting to MongoDB:', err.message));
+};
+
+module.exports = {
+  connectToDatabase,
+};
